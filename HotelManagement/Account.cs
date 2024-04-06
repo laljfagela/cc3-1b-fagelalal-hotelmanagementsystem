7f1b2fd99f704d43a2b392dd3cd40fedbf51a1a7
Lucci Fagela <145944229+laljfagela@users.noreply.github.com>
2024-04-06 17:56:57 +0800

    Update Account.cs

--- HotelManagement/Account.cs
+++ HotelManagement/Account.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,28 @@ namespace HotelManagement
 {
     public class Account
     {
-        private int s_accountSeed = 0; 
-        public int AccountPassword { get; set; }
+        private static int s_accountSeed = 0;
+        private int AccountPassword { get; set; }
+
+        public Account()
+        {
+            AccountPassword = GenerateRandomPassword();
+        }
+
+        private int GenerateRandomPassword()
+        {
+            s_accountSeed++;
+            return s_accountSeed;
+        }
+
+        public bool CheckPassword(int password)
+        {
+            return AccountPassword == password;
+        }
+
+        public void SetPassword(int password)
+        {
+            AccountPassword = password;
+        }
     }
 }
